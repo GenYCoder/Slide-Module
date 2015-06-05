895817edfff6cb5e1c735b2f65953943b54d9339
Sumen Tse <tses@mskcc.org>
2015-06-05 11:55:24 -0400

    adding better coding convention

--- controllers/setup.js
+++ controllers/setup.js
@@ -1,100 +1,96 @@
-angular.module("setup",[])
-	.controller("setupCtrl", function($scope, $http, $location, initializer, spService, imageStorage, configurationList){
-		
-
-		var config = null;
-		$scope.folderURL = "";
+angular.module("setup", [])
+    .controller("setupCtrl", function ($scope, $http, $location, initializer, spService, imageStorage, configurationList) {
 
 
-
-		$scope.save = function(){
-
-			//the list database should be setup with these names
-			var data = {
-				PageURL: initializer.Url.main,
-				SlideFolder: ($scope.folderURL).replace(/(^[^A-Za-z0-9]+|[^A-Za-z0-9]+$)/g,""),
-				encodeURL: initializer.encodeURL,
-				options: JSON.stringify({
-					headline: $scope.options.headline,
-					labelheadline: $scope.options.labelheadline
-				})
-			};
-
-			//check if slide Folder exists and if it contains images
-			$http({
-				url:"https://" + initializer.Url.host + initializer.Url.base + "/_api/web/getFolderByServerRelativeUrl('" + initializer.Url.base + data.SlideFolder + "')/Files?$select=Title,Name,TimeCreated,TimeLastModified,ListItemAllFields/Description0, ListItemAllFields/OData__Author&$expand=Author,ListItemAllFields",
-		        method: "GET",
-		        headers: { "Accept": "application/json; odata=verbose"}
-			}).then(function(response){
-
-				//obtaining json structure of images
-				imageStorage.add(response.data.d.results);
-				imageStorage.setFolderLocation(data.SlideFolder);
-				
-
-				//If there's no images then it will not save the data on to the configuration because the folder does not exist or it doesn't have any images at all
-				if(imageStorage.get().length === 0){
-					
-				}else{
-					
-					
-					
-
-					//check slideshow configuration to see if it needs to be added or updated
-					spService.getListItems("https://" + initializer.Url.host + initializer.Url.base, configurationList, "?$select=ID, PageURL, SlideFolder, encodeURL, options&$filter=encodeURL eq '" + initializer.encodeURL + "'",
-						function(response){
-							
-							config = response.data.d.results;
-							
-							//the condition for adding or updating
-
-							if( config.length > 0 ){
-								
-								spService.updateListItem("https://" + initializer.Url.host + initializer.Url.base, configurationList, config[0].ID, data,
-									function(response){
-
-										imageStorage.setIndex(1);
-										imageStorage.setOptions(JSON.parse(data.options));
-										$location.path("/slide/1");
-
-									}, function(response){
-										//fail to update the item on the slideshow configuration list
-									}
-								);
-
-							}else{
-
-								spService.addListItem("https://" + initializer.Url.host + initializer.Url.base, configurationList, data,
-									function(response){
+        var config = null;
+        $scope.folderURL = "";
 
 
-										imageStorage.setIndex(1);
-										imageStorage.setOptions(JSON.parse(data.options));
-										$location.path("/slide/1");
-										
-									},
-									function(response){
-										//fail to add the item onto the slideshow configuration list
-									}
-								);
-								
-							}
 
-						},
-						function(response){
-							//fail to get the slideshow configuration list
-						}
-					);
+        $scope.save = function () {
+
+            //the list database should be setup with these names
+            var data = {
+                PageURL: initializer.Url.main,
+                SlideFolder: ($scope.folderURL).replace(/(^[^A-Za-z0-9]+|[^A-Za-z0-9]+$)/g, ""),
+                encodeURL: initializer.encodeURL,
+                options: JSON.stringify({
+                    headline: $scope.options.headline,
+                    labelheadline: $scope.options.labelheadline
+                })
+            };
+
+            //check if slide Folder exists and if it contains images
+            $http({
+                url: "https://" + initializer.Url.host + initializer.Url.base + "/_api/web/getFolderByServerRelativeUrl('" + initializer.Url.base + data.SlideFolder + "')/Files?$select=Title,Name,TimeCreated,TimeLastModified,ListItemAllFields/Description0, ListItemAllFields/OData__Author&$expand=Author,ListItemAllFields",
+                method: "GET",
+                headers: {
+                    "Accept": "application/json; odata=verbose"
+                }
+            }).then(function (response) {
+
+                //obtaining json structure of images
+                imageStorage.add(response.data.d.results);
+                imageStorage.setFolderLocation(data.SlideFolder);
 
 
-				}
-
-			}, function(response){
-
-			})
+                //If there's no images then it will not save the data on to the configuration because the folder does not exist or it doesn't have any images at all
+                if (imageStorage.get().length !== 0) {
 
 
-			
-		}
+                    //check slideshow configuration to see if it needs to be added or updated
+                    spService.getListItems("https://" + initializer.Url.host + initializer.Url.base, configurationList, "?$select=ID, PageURL, SlideFolder, encodeURL, options&$filter=encodeURL eq '" + initializer.encodeURL + "'",
+                        function (response) {
 
-	})
+                            config = response.data.d.results;
+
+                            //the condition for adding or updating
+
+                            if (config.length > 0) {
+
+                                spService.updateListItem("https://" + initializer.Url.host + initializer.Url.base, configurationList, config[0].ID, data,
+                                    function (response) {
+
+                                        imageStorage.setIndex(1);
+                                        imageStorage.setOptions(JSON.parse(data.options));
+                                        $location.path("/slide/1");
+
+                                    },
+                                    function (response) {
+                                        //fail to update the item on the slideshow configuration list
+                                    });
+
+                            } else {
+
+                                spService.addListItem("https://" + initializer.Url.host + initializer.Url.base, configurationList, data,
+                                    function (response) {
+
+
+                                        imageStorage.setIndex(1);
+                                        imageStorage.setOptions(JSON.parse(data.options));
+                                        $location.path("/slide/1");
+
+                                    },
+                                    function (response) {
+                                        //fail to add the item onto the slideshow configuration list
+                                    });
+
+                            }
+
+                        },
+                        function (response) {
+                            //fail to get the slideshow configuration list
+                        });
+
+
+                }
+
+            }, function (response) {
+
+            });
+
+
+
+        };
+
+    });
--- controllers/slide.js
+++ controllers/slide.js
@@ -1,86 +1,86 @@
 angular.module("slide", [])
-	.controller("slideShowCtrl", function($scope, $location, $routeParams, $ngSilentLocation, $rootScope, imageStorage, initializer){
+    .controller("slideShowCtrl", function ($scope, $location, $routeParams, $ngSilentLocation, imageStorage, initializer) {
 
-		$scope.slides = [];
-		$scope.currentSlideNum = "";
-		$scope.totalSlides = "";
-		$scope.options = {};
-		$scope.Url = initializer.Url;
-		$scope.slideLocation = null;
-
-
-		//if no image then run to get the image
-		if(imageStorage.get() === null){
-			
-			initializer.run().then(function(response){
-				$scope.slides = imageStorage.get();
-				$scope.options = imageStorage.getOptions();
-
-				//because the folder doesn't exist it will go back to setup mode
-				if( $scope.slides.length === 0 ){
-					$location.path("/setup");
-				}
-
-				$scope.currentSlideNum = parseInt($routeParams.index);
-				$scope.totalSlides = $scope.slides.length;
-				$scope.slideLocation = imageStorage.getFolderLocation();
-
-				imageStorage.setIndex($scope.currentSlideNum);
-
-			})
-		}else{
-			
-			$scope.slides = imageStorage.get();
-			$scope.options = imageStorage.getOptions();
-			$scope.slideLocation = imageStorage.getFolderLocation();
-
-			
-
-			if(parseInt($routeParams.index) <= $scope.slides.length){
-				$scope.currentSlideNum = parseInt($routeParams.index);
-				
-				//no need to set the index if its the same
-				if($scope.currentSlideNum !== imageStorage.getIndex()){
-					imageStorage.setIndex($scope.currentSlideNum);
-				}
-
-			}else{
-				//go back to original because that index does not exist
-				$scope.currentSlideNum = imageStorage.getIndex();
-				$location.path("/slide/" + $scope.currentSlideNum);
-			}
-
-			$scope.totalSlides = $scope.slides.length;
-		}
+        $scope.slides = [];
+        $scope.currentSlideNum = "";
+        $scope.totalSlides = "";
+        $scope.options = {};
+        $scope.Url = initializer.Url;
+        $scope.slideLocation = null;
 
 
-		$scope.leftArrow = function(){
-			if( ($scope.currentSlideNum - 1) <= 0 ){
-				
-				$scope.currentSlideNum = $scope.totalSlides;
-				imageStorage.setIndex($scope.currentSlideNum);
-				$ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
+        //if no image then run to get the image
+        if (imageStorage.get() === null) {
 
-			}else{
-				
-				$scope.currentSlideNum -= 1;
-				imageStorage.setIndex($scope.currentSlideNum);
-				$ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
-				
-			}
-		}
+            initializer.run().then(function () {
+                $scope.slides = imageStorage.get();
+                $scope.options = imageStorage.getOptions();
 
-		$scope.rightArrow = function(){
-			
-			if( ($scope.currentSlideNum + 1) > $scope.totalSlides){
-				$scope.currentSlideNum = 1;
-				imageStorage.setIndex($scope.currentSlideNum);
-				$ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
-			}else{
-				$scope.currentSlideNum += 1;
-				imageStorage.setIndex($scope.currentSlideNum);
-				$ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
-				
-			}
-		}
-	})
+                //because the folder doesn't exist it will go back to setup mode
+                if ($scope.slides.length === 0) {
+                    $location.path("/setup");
+                }
+
+                $scope.currentSlideNum = parseInt($routeParams.index, 10);
+                $scope.totalSlides = $scope.slides.length;
+                $scope.slideLocation = imageStorage.getFolderLocation();
+
+                imageStorage.setIndex($scope.currentSlideNum);
+
+            });
+        } else {
+
+            $scope.slides = imageStorage.get();
+            $scope.options = imageStorage.getOptions();
+            $scope.slideLocation = imageStorage.getFolderLocation();
+
+
+
+            if (parseInt($routeParams.index, 10) <= $scope.slides.length) {
+                $scope.currentSlideNum = parseInt($routeParams.index, 10);
+
+                //no need to set the index if its the same
+                if ($scope.currentSlideNum !== imageStorage.getIndex()) {
+                    imageStorage.setIndex($scope.currentSlideNum);
+                }
+
+            } else {
+                //go back to original because that index does not exist
+                $scope.currentSlideNum = imageStorage.getIndex();
+                $location.path("/slide/" + $scope.currentSlideNum);
+            }
+
+            $scope.totalSlides = $scope.slides.length;
+        }
+
+
+        $scope.leftArrow = function () {
+            if (($scope.currentSlideNum - 1) <= 0) {
+
+                $scope.currentSlideNum = $scope.totalSlides;
+                imageStorage.setIndex($scope.currentSlideNum);
+                $ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
+
+            } else {
+
+                $scope.currentSlideNum -= 1;
+                imageStorage.setIndex($scope.currentSlideNum);
+                $ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
+
+            }
+        };
+
+        $scope.rightArrow = function () {
+
+            if (($scope.currentSlideNum + 1) > $scope.totalSlides) {
+                $scope.currentSlideNum = 1;
+                imageStorage.setIndex($scope.currentSlideNum);
+                $ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
+            } else {
+                $scope.currentSlideNum += 1;
+                imageStorage.setIndex($scope.currentSlideNum);
+                $ngSilentLocation.silent("/slide/" + $scope.currentSlideNum);
+
+            }
+        };
+    });
